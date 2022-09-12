e59c3c63a0a28f7f4d9dd95045e9088f414bff27
Rodney McGee <rodneymcgee73@gmail.com>
2022-09-12 14:01:59 -0500

    Copy and Pasted Code from Dcc-React-Tutorial for Reference

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
+
 
 
 function App() {
+
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
+
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
-      <h3>Hello World!</h3>
+      <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
+
 
 export default App;
